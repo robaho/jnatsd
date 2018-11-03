28f2d4e3e4cc05ad533d2f3b824f64f006f7b527
robaho <rengels@ix.netcom.com>
2018-11-03 12:49:38 -0500

    remove synchronization

--- src/main/util/RingBuffer.java
+++ src/main/util/RingBuffer.java
@@ -1,41 +1,42 @@
 package com.robaho.jnatsd.util;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
 /**
  * ring buffer designed for multiple writers and a single reader
  *
  * @param <T>
  */
 public class RingBuffer<T> {
-    private final T[] ring;
-    private volatile int head;
-    private volatile int tail;
+    private final AtomicReferenceArray<T> ring;
+    private int head;
+    private final AtomicInteger tail = new AtomicInteger();
+    private final int size;
 
     public RingBuffer(int size){
-        ring = (T[]) new Object[size];
+        ring = new AtomicReferenceArray<>(size);
+        this.size=size;
     }
 
-    public synchronized boolean offer(T t) {
-        int n = next(tail);
-        if(n!=head) {
-            ring[tail]=t;
-            tail=n;
-            return true;
+    public boolean offer(T t) {
+        int _tail = tail.get();
+        if(ring.get(_tail)==null){
+            if(tail.compareAndSet(_tail,next(_tail))){
+                return ring.compareAndSet(_tail,null,t);
+            }
         }
+
         return false;
     }
     public T poll() {
-        if(head==tail){
+        T tmp = ring.getAndSet(head,null);
+        if(tmp==null)
             return null;
-        }
-        T tmp = ring[head];
-        ring[head]=null;
-        head = next(head);
+        head=next(head);
         return tmp;
     }
-    public boolean isEmpty() {
-        return head==tail;
-    }
     private int next(int index) {
-        return (++index)%ring.length;
+        return (++index)%size;
     }
 }
