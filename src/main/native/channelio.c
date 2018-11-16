fb0860b6a694fc9f929e46c60226b9b77db0e83e
robaho <rengels@ix.netcom.com>
2018-11-16 00:19:43 -0600

    use JavaCritical since calls are non-blocking

--- src/main/native/channelio.c
+++ src/main/native/channelio.c
@@ -3,23 +3,28 @@
 #include <unistd.h>
 #include <errno.h>
 #include <poll.h>
+#include <stdio.h>
 #include "com_robaho_jnatsd_util_ChannelIO.h"
 
-JNIEXPORT jint JNICALL Java_com_robaho_jnatsd_util_ChannelIO_write0(JNIEnv *jenv, jclass clazz, jint fd, jlong address, jint len) {
+JNIEXPORT jint JNICALL JavaCritical_com_robaho_jnatsd_util_ChannelIO_write0(jint fd, jlong address, jint len) {
       int n = write(fd,(void*)address,len);
       if(n<0)
          return -1*errno;
-      return n;   
+      return n;
 }
 
-/*
- * Class:     com_robaho_jnatsd_util_ChannelIO
- * Method:    read0
- * Signature: (IJI)I
- */
-JNIEXPORT jint JNICALL Java_com_robaho_jnatsd_util_ChannelIO_read0(JNIEnv *jenv, jclass clazz, jint fd, jlong address, jint len){
-    int n = read(fd,(void*)address,len);
+JNIEXPORT jint JNICALL Java_com_robaho_jnatsd_util_ChannelIO_write0(JNIEnv *jenv, jclass clazz, jint fd, jlong address, jint len) {
+      return JavaCritical_com_robaho_jnatsd_util_ChannelIO_write0(fd,address,len);
+}
+
+JNIEXPORT jint JNICALL JavaCritical_com_robaho_jnatsd_util_ChannelIO_read0(jint fd, jlong address, jint len){
+      int n = read(fd,(void*)address,len);
       if(n<0)
          return -1*errno;
-      return n;   
+      return n;
 }
+
+JNIEXPORT jint JNICALL Java_com_robaho_jnatsd_util_ChannelIO_read0(JNIEnv *jenv, jclass clazz, jint fd, jlong address, jint len){
+      return JavaCritical_com_robaho_jnatsd_util_ChannelIO_read0(fd,address,len);
+}
+
